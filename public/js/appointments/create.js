a2f790e1aa5f98a9336080ab3843b39b559ef831
Kevin Miranda Luis <kevin151720@gmail.com>
2021-05-18 16:18:41 -0500

    v7

--- public/js/appointments/create.js
+++ public/js/appointments/create.js
@@ -40,8 +40,9 @@ const noHoursAlert = `<div class="alert alert-danger" role="alert">
     }
 
     function displayHours(data){
-        if(!data.morning && !data.afternoon)
+        if(!data.morning && !data.afternoon || data.morning.length ==0 && data.afternoon.length == 0)
         {
+          
           $hours.html(noHoursAlert);
           return;
         }
