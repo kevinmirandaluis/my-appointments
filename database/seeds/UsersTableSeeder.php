538520bb25c1c97bc62e604c3bd617e57e88326b
kevinmirandaluis <a1520358@idat.edu.pe>
2021-05-23 17:54:16 -0500

    Cambio de admin

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,8 +15,8 @@ class UsersTableSeeder extends Seeder
 
 
     	User::create([
-		'name' => 'Kevin Miranda',
-        'email' => 'kevin151720@gmail.com',
+		'name' => 'Admin Denti',
+        'email' => 'admindenti@gmail.com',
         'email_verified_at' => now(),
         'password' => bcrypt('123123'), // password
         'role' => 'admin'
