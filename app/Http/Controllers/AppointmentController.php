7992412ab7b15136103b6a23a64d521ed42dfd16
kevinmirandaluis <a1520358@idat.edu.pe>
2021-06-11 11:04:47 -0500

    enviar notificaciones

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -106,7 +106,10 @@ class AppointmentController extends Controller
     public function postConfirm(Appointment $appointment)
     {
         $appointment->status = 'Confirmada';
-        $appointment->save();
+        $saved = $appointment->save();
+
+        if($saved)
+            $appointment->patient->sendFCM('Su cita se ha confirmado!')
 
         $notification = 'La cita se ha confirmado correctamente';
         return redirect('/appointments')->with(compact('notification'));
@@ -125,6 +128,9 @@ class AppointmentController extends Controller
         
         $appointment->status = 'Cancelada';
         $saved = $appointment->save();
+
+        if($saved)
+            $appointment->patient->sendFCM('Su cita ha sido cancelada.')
 
         $notification = 'La cita se ha cancelado correctamente';
         return redirect('/appointments')->with(compact('notification'));
--- app/User.php
+++ app/User.php
@@ -76,5 +76,16 @@ class User extends Authenticatable
         return $this->hasMany(Appointment::class,'patient_id');
     }
 
-
+    public function sendFCM($message)
+    {
+            return fcm()
+            ->to([
+                $this->device_token
+            ])
+            ->notification([
+                'title' => config('app.name'),
+                'body' => $message
+            ])
+            ->send();
+    }
 }
